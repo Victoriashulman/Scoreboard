4f48c74e7ba1f9d346f2340381aa9cc7c388d505
Victoriashulman <victorias252@nycstudents.net>
2024-10-09 15:48:46 +0000

    commit

--- Scoreboard.java
+++ Scoreboard.java
@@ -17,12 +17,25 @@ public Scoreboard(String one, String two)
     }
     public void recordPlay(int score)
     {
+        if(score == 0){
+               if (activeTeam.equals(teamOne)) activeTeam = teamTwo;
+                else if(activeTeam.equals(teamTwo)) activeTeam = teamOne;
+        }
         if(teamOne.equals(activeTeam)){
-            teamOneScore += score;
+            teamOneScore += score;    
+        }
+        else{
+            teamTwoScore += score;
+        }
+
 
         }
 
 
-}
-}
+        }
+        
+
+
+
+
     
